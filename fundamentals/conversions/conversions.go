0e36bf59980ec811c9aedfed262d94ec356d7044
dgoscn <diego-ii@live.com>
2022-08-07 00:31:44 -0300

    Conversions on golang

---
+++ fundamentals/conversions/conversions.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	x := 2.4
+	y := 2
+	fmt.Println(x / float64(y))
+
+	score := 6.9
+	finalScore := int(score)
+	fmt.Println(finalScore)
+
+	//warning
+	fmt.Println("Test " + string(97)) //ascii value to concatanate
+
+	//int to string
+	fmt.Println("Test" + strconv.Itoa(123))
+
+	//string to int
+	num, _ := strconv.Atoi("123") // num relates to a number and _ to a possible error
+	fmt.Println(num - 122)
+
+	b, _ := strconv.ParseBool("true")
+	if b {
+		fmt.Println("True")
+	}
+}
