d76c41774c283153e576b436440657fbaf2a1dff
dgoscn <diego-ii@live.com>
2022-08-06 21:46:02 -0300

    inicial go commit

---
+++ fundamentals/first/first.go
@@ -0,0 +1,13 @@
+// Executable programs are initiated by main package
+package main
+
+/* Golang code, are organized in packages
+and to make the use of them, it is needed a declaration of many imports
+*/
+import "fmt"
+
+//Entry door for a Program in Go
+func main() {
+	fmt.Print("First ")
+	fmt.Print("Program!!!")
+}
